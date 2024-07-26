c4886fd40070f02c4e9a33f80da7bfc4d8ffb43b
Jonathan Perkin <jonathan@perkin.org.uk>
2024-07-26 09:43:25 +0100

    Flesh out makesum argument parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
+/*
+ * Copyright (c) 2024 Jonathan Perkin <jonathan@perkin.org.uk>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    /// Enable verbose output
+    #[arg(short, long)]
+    verbose: bool,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+#[command(rename_all = "lower")]
+enum Commands {
+    /// Create or update distinfo file
+    MakeSum {
+        #[arg(short = 'a', value_name = "algorithm")]
+        #[arg(help = "Algorithm digests to create for each distfile")]
+        dalgorithms: Vec<String>,
+
+        #[arg(short, value_name = "distfile")]
+        #[arg(help = "Generate digest for each named distfile")]
+        cksumfile: Vec<PathBuf>,
+
+        #[arg(short, value_name = "distdir")]
+        #[arg(help = "Directory under which distfiles are found")]
+        distdir: Option<PathBuf>,
+
+        #[arg(short = 'f', value_name = "distinfo")]
+        #[arg(help = "Path to an existing distinfo file")]
+        distinfo: Option<PathBuf>,
+
+        #[arg(short = 'I', value_name = "input")]
+        #[arg(help = "Read distfiles from input instead of -c")]
+        input: Option<PathBuf>,
+
+        #[arg(short, value_name = "ignorefile")]
+        #[arg(help = "List of distfiles to ignore (unused)")]
+        ignorefile: Option<PathBuf>,
+
+        #[arg(short = 'p', value_name = "algorithm")]
+        #[arg(help = "Algorithm digests to create for each patchfile")]
+        palgorithms: Vec<String>,
+
+        #[arg(value_name = "patch")]
+        #[arg(help = "Alphabetical list of named patch files")]
+        patches: Vec<PathBuf>,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    dbg!(&args);
 }
